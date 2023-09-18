79bda51d12a2a4e5bd9cfbd62a80511b33e82e2a
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2023-09-18 18:24:07 +0530

    Update Sort_Array_using_Recursion.java
    Code_Updated

--- Recursion/Sort_Array_using_Recursion.java
+++ Recursion/Sort_Array_using_Recursion.java
@@ -4,7 +4,7 @@ Problem Statement : Sort and Array Using Recursion ..
 TC : O(n^2)    SC : O(1)  --> but recursive call use Auxillary Space..
 */
 
-
+//Version :1
 import java.util.*;
 public class Main
 {
@@ -40,3 +40,43 @@ public class Main
 	    System.out.println(list);
 	}
 }
+
+//Version :02
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(3,5,6,-8,9,12,0));
+	    sortArrayRecurssion(list);
+	    System.out.println(list);
+		
+	}
+	public static void sortArrayRecurssion(ArrayList<Integer> arr){
+	    //Base Case :::
+	    if(arr.size()==1){
+	        return ;
+	    }
+	    //Hypothesis:::
+	    int val = arr.get(arr.size()-1);
+	    arr.remove(arr.size()-1);
+	    //Induction
+	    sortArrayRecurssion(arr);
+	    insertEle(arr,val);
+	}
+	public static void insertEle(ArrayList<Integer> arr,int temp){
+	    //Base Case ::
+	    if(arr.size()==0 || arr.get(arr.size()-1)<=temp){
+	        arr.add(temp);
+	        return;
+	    }
+	    //Hyposthesis..
+	    int last =arr.get(arr.size()-1);
+	    arr.remove(arr.size()-1);
+	    //Induction..
+	    insertEle(arr,temp);
+	    arr.add(last);
+	}
+}
+
+
+
